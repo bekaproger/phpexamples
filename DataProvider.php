a78cebe33d0ece967070c0dfc5d8eb8cc917384c
Bekaproger <bekanakk@gmail.com>
2019-04-14 18:56:00 +0500

    added test DataProvider

---
+++ DataProvider.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace src\Integration;
+
+use DateTime;
+use Exception;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
+
+class DataProvider
+{
+	private $host;
+	private $user;
+	private $password;
+
+	/*
+	* @param $host
+	* @param $user
+	* @param $password
+	*/
+	public function __construct($params, CacheItemPoolInterface $cache, LoggerInterface $logger)
+	{
+		$this->logger = $logger;
+		$this->params = $this->makeParams($params);
+		$this->cache = $cache;
+	}
+
+	/*
+	* From Request
+	* 
+	* @param array $request
+	* @return array
+	*/
+	protected function makeRequest(array $request)
+	{
+		//Make request format
+	}
+
+	/*
+	* Send Request
+	*
+	* @param array $request
+	*/
+	protected function sendRequest(array $request)
+	{
+		try {
+			$request = $this->makeRequest($request);
+			$cacheKey = $this->getCacheKey($request);
+			$cacheItem = $this->cache->getItem($cacheKey);
+			if ($cacheItem->isHit()) {
+				return $cacheItem->get();
+			}
+
+			$result = $this->sendRequest($request);
+
+			$result = $this->makeResponse($result);
+
+			$cacheItem->set($result)->expiresAt((new DateTime())->modify('+1 day'));
+
+			return $result;
+		} catch (Exception $e) {
+			$this->logger->critical('Error');
+			throw new Exception($e->getMessage(), $e->getCode());
+		}
+	}
+
+
+	/*
+	* Form Response
+	*
+	* @param $response
+	*/
+	protected function makeResponse($response)
+	{
+		// Form Response object
+	}
+
+	/*
+	* Form Params
+	*
+	* @param array $params
+	*/
+	protected function makeParams(array $params)
+	{
+		// From Params
+	}
+
+	/*
+	* Form Cache key
+	*
+	* @param array $input
+	* @return string 
+	*/
+	public function makeCacheKey(array $input)
+	{
+		return json_encode($input);
+	}
+
+}
+
+
